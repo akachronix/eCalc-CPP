c3ddee13dcf21ab78133a2461412d1827a01295d
Brendan Gowen <bgowengaming1@gmail.com>
2016-11-17 00:12:13 -0500

    Used a for loop instead of a while loop

--- eCalc.cpp
+++ eCalc.cpp
@@ -47,34 +47,30 @@ long double Multiply(long double a, long double b)
 int main(void)
 {
 	Greeting();
-	
+
 	cout.precision(100);
-	
+
 	string Question1 = "What number do you what to multiply?";
 	string Question2 = "To what power?";
-	
+
 	long double userNum;
 	long double userNum2;
 	long double userExponent;
-	
+
 	userNum = GetValue(Question1);
 	userNum2 = userNum;
 	userExponent = GetValue(Question2);
-	
-	long double i;
-	i = 1;
-	
-	while (i < userExponent)
+
+	for(long double i = 1; i < userExponent; i++)
 	{
 		userNum = userNum * userNum2;
-		i++;
 	}
-	
+
 	cout << "The answer is: " << userNum << endl;
-	
+
 	Ending();
-	
+
 	Delay(2500);
-	
+
 	return 0;
 }
